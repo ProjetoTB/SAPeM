a2e8e1641b336757c897c95810ffeb682a1fcb56
Andressa Sivolella <asivolella@gruyere.lps.ufrj.br>
2012-11-05 12:00:32 -0200

    Ignoring scripts inside 'scripts/' folder

--- scripts/change_values.py
+++ scripts/change_values.py
@@ -2,19 +2,17 @@ from forms.models import Ficha, Paciente
 from xml.dom.minidom import parseString, getDOMImplementation
 impl = getDOMImplementation()
 
-tag = 'diagnostico'
+tag = 'resultadoGenXpert'
 
-fichas = Ficha.objects.filter(formulario__tipo__nome='Consulta')
+fichas = Ficha.objects.filter(formulario__tipo__nome='Exames')
 
 wrong_registers = []
 
 
 fix = { 
-	"melhora"    : "tb_pulmonar",
-	"inalterado" : "tb_extra-pulmonar",
-	"piorado"    : "tb_pulmonar-extra",
-	"nao_tb"     : "nao_tb",
-	"ignorado"   : "ignorado"
+	"positivo"    : "detectavel",
+	"negativo"    : "naoDetectavel",
+	"ignorado"    : "ignorado"
 }
 
 for f in fichas:
@@ -26,6 +24,7 @@ for f in fichas:
 		pass
 
 print 'Found ', len(wrong_registers), ' wrong registers'
+"""
 for f in wrong_registers:
 	print 'Fixing ficha:', f.id
 	xml = parseString(f.conteudo.encode('utf-8'))
@@ -42,3 +41,4 @@ for f in wrong_registers:
 	f.conteudo = new_xml.toxml()
 	f.save()
 	del new_xml, xml
+"""
